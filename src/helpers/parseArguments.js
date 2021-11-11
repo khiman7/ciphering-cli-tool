6f05a1bdc1c332b93d667345440e2cbb65a62ad8
kyrylkhiman@gmail.com <kyrylo.khiman@hneu.net>
2021-11-12 00:32:01 +0200

    refactor(parseArguments): update parseArguments function

--- src/helpers/parseArguments.js
+++ src/helpers/parseArguments.js
@@ -1,16 +1,32 @@
 const { DuplicateArgumentsError } = require('../errors');
+const { availableOptions } = require('../constants');
 
 const parseArguments = () => {
   const args = process.argv.slice(2);
   const options = {};
-  
+
   const isOptionDuplicated = (option) => {
+    /**
+     * If the option is an available option, check for duplicates
+     * including short and full name (e.g. 'c' and 'config').
+     */
+    const availableOption = Object.values(availableOptions).find(
+      (opt) => opt.alias === option || opt.fullName === option
+    );
+
+    if (availableOption) {
+      return (
+        options.hasOwnProperty(availableOption.alias) ||
+        options.hasOwnProperty(availableOption.fullName)
+      );
+    }
+
     return options.hasOwnProperty(option);
   };
 
   args.forEach((arg, index) => {
-    if (arg.startsWith('-')) {
-      const option = arg.slice(1);
+    if (arg.startsWith('--')) {
+      const option = arg.slice(2);
 
       if (isOptionDuplicated(option)) {
         throw new DuplicateArgumentsError(arg);
@@ -18,8 +34,8 @@ const parseArguments = () => {
 
       options[option] = args[index + 1];
       return;
-    } else if (arg.startsWith('--')) {
-      const option = arg.slice(2);
+    } else if (arg.startsWith('-')) {
+      const option = arg.slice(1);
 
       if (isOptionDuplicated(option)) {
         throw new DuplicateArgumentsError(arg);
