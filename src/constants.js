ac84f2b2e1794b83fed4cc0436a4d47685011f08
kyrylkhiman@gmail.com <kyrylo.khiman@hneu.net>
2021-11-12 00:30:35 +0200

    feat: add available options in constants file

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+const availableOptions = {
+  config: {
+    alias: 'c',
+    fullName: 'config',
+  },
+  input: {
+    alias: 'i',
+    fullName: 'input',
+  },
+  output: {
+    alias: 'o',
+    fullName: 'output',
+  },
+};
+
+module.exports = { 
+  availableOptions,
+};
