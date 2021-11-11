e3a1285c5b0ca1e28577bf4b19e97fff72c6abc8
kyrylkhiman@gmail.com <kyrylo.khiman@hneu.net>
2021-11-12 00:33:56 +0200

    chore(errors): change custom error message

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,7 +1,12 @@
 class DuplicateArgumentsError extends Error {
   constructor(argument) {
     super(
-      `Duplicate argument found: ${argument}\nArguments shouldn't be duplicated`
+      `Duplicate argument found: [${argument}]\n`
+      + 'Arguments shouldn\'t be duplicated\n'
+      + 'Available options:\n'
+      + '\t-c, --config: config for ciphers\n'
+      + '\t-i, --input: a path to input file\n'
+      + '\t-o, --output: a path to output file\n'
     );
     this.name = this.constructor.name;
   }
