c901d14ffac991fe6acc7a0c7802283cd227a35d
Kyrylo Khiman <kyrylkhiman@gmail.com>
2021-11-21 19:37:02 +0200

    refactor: add function to test error message

--- __tests__/errorMessages.test.js
+++ __tests__/errorMessages.test.js
@@ -1,72 +1,46 @@
 const path = require('path');
 const { spawn } = require('child_process');
 
+const { MissingConfigArgumentError } = require('../src/errors');
+
+const testErrorMessage = (args, regex) => {
+  const filePath = path.join(__dirname, '../ciphering-tool.js');
+  const testApp = spawn(
+      'node', [filePath, ...args]
+    );
+  let errorMessage = '';
+
+  testApp.stderr.on('data', data => {
+    errorMessage += data.toString();
+  });
+
+  testApp.stderr.on('end', () => {
+    expect(errorMessage).toMatch(regex);
+  });
+}
+
 describe('process arguments', () => {
   test('should show MissingConfigArgumentError message', () => {
-    const filePath = path.join(__dirname, '../ciphering-tool.js');
-    const testApp = spawn(
-      'node', [filePath, '-i', './input.txt', '-o', './output.txt']
-    );
+    const args = ['-i', './input.txt', '-o', './output.txt']
 
-    let res = '';
-
-    testApp.stderr.on('data', data => {
-      res += data.toString();
-    });
-
-    testApp.stderr.on('end', () => {
-      expect(res).toMatch(/^Missing required config argument?/);
-    });
+    testErrorMessage(args, /^Missing required config argument?/);
   });
 
   test('should show FileDoesNotExistError message', () => {
-    const filePath = path.join(__dirname, '../ciphering-tool.js');
-    const testApp = spawn(
-      'node', [filePath, '-c', 'C1', '-i', './test.txt', '-o', './output.txt']
-    );
+    const args = ['-c', 'C1', '-i', './test.txt', '-o', './output.txt'];
 
-    let res = '';
-
-    testApp.stderr.on('data', data => {
-      res += data.toString();
-    });
-
-    testApp.stderr.on('end', () => {
-      expect(res).toMatch(/^File ".\/test.txt"?/);
-    });
+    testErrorMessage(args, /^File ".\/test.txt"?/);
   });
 
   test('should show FileDoesNotExistError message', () => {
-    const filePath = path.join(__dirname, '../ciphering-tool.js');
-    const testApp = spawn(
-      'node', [filePath, '-c', 'C1', '-i', './input.txt', '-o', './test.txt']
-    );
+    const args = ['-c', 'C1', '-i', './input.txt', '-o', './test.txt'];
 
-    let res = '';
-
-    testApp.stderr.on('data', data => {
-      res += data.toString();
-    });
-
-    testApp.stderr.on('end', () => {
-      expect(res).toMatch(/^File ".\/test.txt"?/);
-    });
+    testErrorMessage(args, /^File ".\/test.txt"?/);
   });
 
   test('should show NotValidConfigError message', () => {
-    const filePath = path.join(__dirname, '../ciphering-tool.js');
-    const testApp = spawn(
-      'node', [filePath, '-c', 'R2_D2!', '-i', './input.txt', '-o', './output.txt']
-    );
+    const args = ['-c', 'R2_D2!', '-i', './input.txt', '-o', './output.txt'];
 
-    let res = '';
-      
-    testApp.stderr.on('data', data => {
-      res += data.toString();
-    });
-
-    testApp.stderr.on('end', () => {
-      expect(res).toMatch(/^Config you provided "R2_D2!" is not valid?/);
-    });
+    testErrorMessage(args, /^Config you provided "R2_D2!" is not valid?/);
   });
 });
